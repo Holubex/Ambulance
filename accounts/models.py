a9367025a0f2c539c7828c610d00b3e2ef78308f
DrenaGit <drena.cvorovic@gmail.com>
2024-08-10 10:54:04 +0200

    login.html and register.html extended with base.html, small model changes. All html files were reformated by PyCharm (code).

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,6 @@ from django.db.models import Model, OneToOneField, CASCADE, TextField, DateField
 class Profile(Model):
     user = OneToOneField(User, on_delete=CASCADE)
     birth_date = DateField(null=True, blank=True)
-    biography = TextField(null=True, blank=True)
 
     class Meta:
         ordering = ['user__username']
