55bc70dd120a71a65e49907ebbe9a3bfeaf4c2d8
DrenaGit <drena.cvorovic@gmail.com>
2024-08-10 16:40:27 +0200

    Upgraded design for medical_examinationa_list.html (added filter buttons).

--- medical_examination/views.py
+++ medical_examination/views.py
@@ -1,8 +1,51 @@
+from django import forms
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from medical_examination.forms import MedicalExaminationForm, AnnouncementForm
 from medical_examination.models import MedicalExamination, Announcement
+from patients.models import User, Role
+
+
+# medical_examination
+
+class MedicalExaminationFilterForm(forms.Form):
+    patient = forms.ModelChoiceField(
+        queryset=User.objects.filter(role_patient=Role.PATIENT),  # Přizpůsobte filtrování
+        required=False,
+        label='Pacient',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    nurse = forms.ModelChoiceField(
+        queryset=User.objects.filter(role_patient=Role.NURSE),  # Přizpůsobte filtrování
+        required=False,
+        label='Sestřička',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    doctor = forms.ModelChoiceField(
+        queryset=User.objects.filter(role_patient=Role.DOCTOR),  # Přizpůsobte filtrování
+        required=False,
+        label='Lékař',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    examination_date = forms.DateField(
+        required=False,
+        label='Datum vyšetření',
+        widget=forms.TextInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+
+    def filter(self, queryset):
+        if self.is_valid():
+            data = self.cleaned_data
+            if data.get('patient'):
+                queryset = queryset.filter(patient_id=data['patient'].id)
+            if data.get('nurse'):
+                queryset = queryset.filter(nurse_id=data['nurse'].id)
+            if data.get('doctor'):
+                queryset = queryset.filter(doctor_id=data['doctor'].id)
+            if data.get('examination_date'):
+                queryset = queryset.filter(examination_date=data['examination_date'])
+        return queryset
 
 
 class MedicalExaminationListView(ListView):
@@ -10,6 +53,19 @@ class MedicalExaminationListView(ListView):
     model = MedicalExamination
     context_object_name = 'medical_examinations'
     permission_required = 'medical_examination.view_medical_examination'
+
+    def get_queryset(self):
+        form = MedicalExaminationFilterForm(self.request.GET)
+        queryset = super().get_queryset()
+        if form.is_valid():
+            queryset = form.filter(queryset)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter_form'] = MedicalExaminationFilterForm(self.request.GET)
+        return context
+
 
 class MedicalExaminationCreateView(CreateView):
     template_name = 'medical_examination_form.html'
@@ -29,16 +85,19 @@ class AnnouncementListView(ListView):
     model = Announcement
     context_object_name = 'announcements'
 
+
 class AnnouncementCreateView(CreateView):
     template_name = 'announcement_form.html'
     form_class = AnnouncementForm
     success_url = reverse_lazy('announcement_list')
+
 
 class AnnouncementUpdateView(UpdateView):
     template_name = 'announcement_form.html'
     form_class = AnnouncementForm
     model = Announcement
     success_url = reverse_lazy('announcement_list')
+
 
 class AnnouncementDeleteView(DeleteView):
     template_name = 'announcement_confirm_delete.html'
