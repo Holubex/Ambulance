5b2f53d5d70dcb55c6bbb1af0dc94263a49be5e0
DrenaGit <drena.cvorovic@gmail.com>
2024-08-15 12:14:15 +0200

    New query object filter for searching patients - user_list.html + patients\views.py - imported Q.

--- medical_examination/views.py
+++ medical_examination/views.py
@@ -1,42 +1,48 @@
+# views.py
 from django import forms
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.db.models import Q
 from medical_examination.forms import MedicalExaminationForm, AnnouncementForm
 from medical_examination.models import MedicalExamination, Announcement
-from patients.models import User, Role
+from patients.models import User as Patient, Role
 
 
 # Formulář pro filtrování lékařských vyšetření
 class MedicalExaminationFilterForm(forms.Form):
     patient = forms.ModelChoiceField(
-        queryset=User.objects.filter(role_patient=Role.PATIENT).order_by('surname', 'name'),  # Přizpůsobte filtrování
-        required=False,  # Pole není povinné, uživatel jej nemusí vyplnit
-        label='Pacient',
-        widget=forms.Select(attrs={'class': 'form-control'})  # Stylování pomocí CSS třídy
+        # Výběr pacientů s rolí 'PATIENT', seřazeno podle příjmení a jména
+        queryset=Patient.objects.filter(role_patient=Role.PATIENT).order_by('surname', 'name'),
+        required=False,  # Toto pole není povinné
+        label='Pacient',  # Popis pole, který bude zobrazen v šabloně
+        widget=forms.Select(attrs={'class': 'form-control'})  # Používá CSS třídu 'form-control' pro stylování
     )
     nurse = forms.ModelChoiceField(
-        queryset=User.objects.filter(role_patient=Role.NURSE).order_by('surname', 'name'),
-        required=False,  # Pole není povinné
-        label='Sestřička',
-        widget=forms.Select(attrs={'class': 'form-control'})
+        # Výběr sestřiček s rolí 'NURSE', seřazeno podle příjmení a jména
+        queryset=Patient.objects.filter(role_patient=Role.NURSE).order_by('surname', 'name'),
+        required=False,  # Toto pole není povinné
+        label='Sestřička',  # Popis pole, který bude zobrazen v šabloně
+        widget=forms.Select(attrs={'class': 'form-control'})  # Používá CSS třídu 'form-control' pro stylování
     )
     doctor = forms.ModelChoiceField(
-        queryset=User.objects.filter(role_patient=Role.DOCTOR).order_by('surname', 'name'),
-        required=False,  # Pole není povinné
-        label='Lékař',
-        widget=forms.Select(attrs={'class': 'form-control'})
+        # Výběr lékařů s rolí 'DOCTOR', seřazeno podle příjmení a jména
+        queryset=Patient.objects.filter(role_patient=Role.DOCTOR).order_by('surname', 'name'),
+        required=False,  # Toto pole není povinné
+        label='Lékař',  # Popis pole, který bude zobrazen v šabloně
+        widget=forms.Select(attrs={'class': 'form-control'})  # Používá CSS třídu 'form-control' pro stylování
     )
     examination_date = forms.DateField(
-        required=False,  # Pole není povinné
-        label='Datum vyšetření',
-        widget=forms.TextInput(attrs={'type': 'date', 'class': 'form-control'})  # Textové pole pro výběr data
+        # Výběr data vyšetření
+        required=False,  # Toto pole není povinné
+        label='Datum vyšetření',  # Popis pole, který bude zobrazen v šabloně
+        widget=forms.TextInput(attrs={'type': 'date', 'class': 'form-control'})  # Používá textové pole s typem 'date' pro výběr data
     )
 
     def filter(self, queryset):
         # Filtruje queryset na základě vyplněných polí formuláře
         if self.is_valid():
-            data = self.cleaned_data  # Získá validovaná data z formuláře
+            data = self.cleaned_data
             if data.get('patient'):
                 queryset = queryset.filter(patient_id=data['patient'].id)  # Filtruje podle ID pacienta
             if data.get('nurse'):
@@ -48,103 +54,116 @@ class MedicalExaminationFilterForm(forms.Form):
         return queryset
 
 
-# Zobrazení seznamu lékařských vyšetření
+# Zobrazení seznamu lékařských vyšetření s možností filtrování
 class MedicalExaminationListView(ListView):
-    template_name = 'medical_examination_list.html'
-    model = MedicalExamination  # Model, ze kterého se načítají data
-    context_object_name = 'examinations'  # Název objektu v šabloně
-    permission_required = 'medical_examination.view_medical_examination'  # Požadované oprávnění
+    model = MedicalExamination
+    template_name = "medical_examination_list.html"
+    context_object_name = "examinations"
+
+    def get_context_data(self, **kwargs):
+        # Přidává do kontextu seznam sestřiček a lékařů a formulář pro filtrování
+        context = super().get_context_data(**kwargs)
+        context['nurses'] = Patient.objects.filter(role_patient=Role.NURSE)
+        context['doctors'] = Patient.objects.filter(role_patient=Role.DOCTOR)
+        context['filter_form'] = MedicalExaminationFilterForm(self.request.GET)
+        return context
 
     def get_queryset(self):
-        # Získá a filtruje queryset vyšetření na základě formuláře
-        form = MedicalExaminationFilterForm(self.request.GET)
-        queryset = super().get_queryset().select_related('patient', 'nurse', 'doctor')  # Efektivnější dotazy s relacemi
+        # Získá queryset lékařských vyšetření a aplikuje filtry na základě dotazu
+        queryset = super().get_queryset().select_related('patient', 'nurse', 'doctor')
+        patient_name = self.request.GET.get('patient_name', '')  # Získá jméno pacienta z GET parametrů
+        nurse_id = self.request.GET.get('nurse_id', '')  # Získá ID sestřičky z GET parametrů
+        doctor_id = self.request.GET.get('doctor_id', '')  # Získá ID lékaře z GET parametrů
 
-        if form.is_valid():
-            queryset = form.filter(queryset)  # Aplikace filtrování na queryset
+        # Pokud je zadáno jméno pacienta, filtruje podle příjmení nebo jména pacienta
+        if patient_name:
+            queryset = queryset.filter(
+                Q(patient__surname__icontains=patient_name) |
+                Q(patient__name__icontains=patient_name)
+            )
+        # Pokud je zadáno ID sestřičky, filtruje podle ID sestřičky
+        if nurse_id:
+            queryset = queryset.filter(nurse_id=nurse_id)
+        # Pokud je zadáno ID lékaře, filtruje podle ID lékaře
+        if doctor_id:
+            queryset = queryset.filter(doctor_id=doctor_id)
 
-        queryset = queryset.order_by(
+        # Seřadí výsledky podle příjmení a jména pacienta, sestřičky a lékaře
+        return queryset.order_by(
             'patient__surname', 'patient__name',
             'nurse__surname', 'nurse__name',
             'doctor__surname', 'doctor__name'
-        )  # Seřazení podle příjmení a jména
-        return queryset
-
-    def get_context_data(self, **kwargs):
-        # Přidá formulář pro filtrování do kontextu
-        context = super().get_context_data(**kwargs)
-        context['filter_form'] = MedicalExaminationFilterForm(self.request.GET)  # Formulář pro zobrazení
-        return context
+        )
 
 
 # Zobrazení pro vytvoření nového lékařského vyšetření
 class MedicalExaminationCreateView(CreateView):
     template_name = 'medical_examination_form.html'
-    form_class = MedicalExaminationForm  # Třída formuláře
-    success_url = reverse_lazy('medical_examination_list')  # Přesměrování po úspěšném vytvoření
-    permission_required = 'medical_examination.add_medical_examination'  # Požadované oprávnění
+    form_class = MedicalExaminationForm
+    success_url = reverse_lazy('medical_examination_list')
+    permission_required = 'medical_examination.add_medical_examination'
 
 
 # Zobrazení detailu lékařského vyšetření
 class MedicalExaminationDetailView(DetailView):
     template_name = 'medical_examination_detail.html'
-    model = MedicalExamination  # Model, ze kterého se zobrazují detaily
-    context_object_name = 'medical_examination'  # Název objektu v šabloně
+    model = MedicalExamination
+    context_object_name = 'medical_examination'
 
 
 # Zobrazení pro aktualizaci lékařského vyšetření
 class MedicalExaminationUpdateView(UpdateView):
     template_name = 'medical_examination_form.html'
-    form_class = MedicalExaminationForm  # Třída formuláře
-    model = MedicalExamination  # Přidává model pro identifikaci vyšetření
-    success_url = reverse_lazy('medical_examination_list')  # Přesměrování po úspěšné aktualizaci
-    permission_required = 'medical_examination.change_medical_examination'  # Požadované oprávnění
+    form_class = MedicalExaminationForm
+    model = MedicalExamination
+    success_url = reverse_lazy('medical_examination_list')
+    permission_required = 'medical_examination.change_medical_examination'
 
     def get_context_data(self, **kwargs):
         # Přidá do kontextu název akce pro formulář
         context = super().get_context_data(**kwargs)
-        context['form_action'] = 'Upraviť vyšetření'  # Popis akce ve formuláři
+        context['form_action'] = 'Upravit vyšetření'
         return context
 
 
 # Zobrazení pro smazání lékařského vyšetření
 class MedicalExaminationDeleteView(DeleteView):
     template_name = 'medical_examination_confirm_delete.html'
-    model = MedicalExamination  # Určuje model pro smazání
-    success_url = reverse_lazy('medical_examination_list')  # Přesměrování po úspěšném smazání
-    permission_required = 'medical_examination.delete_medical_examination'  # Požadované oprávnění
+    model = MedicalExamination
+    success_url = reverse_lazy('medical_examination_list')
+    permission_required = 'medical_examination.delete_medical_examination'
 
     def get_context_data(self, **kwargs):
-        # Přidá do kontextu název akce pro formulář
+        # Přidá do kontextu název akce pro potvrzení smazání
         context = super().get_context_data(**kwargs)
-        context['form_action'] = 'Vymazat vyšetření'  # Popis akce ve formuláři
+        context['form_action'] = 'Vymazat vyšetření'
         return context
 
 
 # Zobrazení seznamu oznámení
 class AnnouncementListView(ListView):
     template_name = 'announcement_list.html'
-    model = Announcement  # Model pro načítání oznámení
-    context_object_name = 'announcements'  # Název objektu v šabloně
+    model = Announcement
+    context_object_name = 'announcements'
 
 
 # Zobrazení pro vytvoření nového oznámení
 class AnnouncementCreateView(CreateView):
     template_name = 'announcement_form.html'
-    form_class = AnnouncementForm  # Třída formuláře pro oznámení
-    success_url = reverse_lazy('announcement_list')  # Přesměrování po úspěšném vytvoření
+    form_class = AnnouncementForm
+    success_url = reverse_lazy('announcement_list')
 
 
 # Zobrazení pro aktualizaci oznámení
 class AnnouncementUpdateView(UpdateView):
     template_name = 'announcement_form.html'
-    form_class = AnnouncementForm  # Třída formuláře pro oznámení
-    model = Announcement  # Model pro načítání a ukládání oznámení
-    success_url = reverse_lazy('announcement_list')  # Přesměrování po úspěšné aktualizaci
+    form_class = AnnouncementForm
+    model = Announcement
+    success_url = reverse_lazy('announcement_list')
 
 
 # Zobrazení pro smazání oznámení
 class AnnouncementDeleteView(DeleteView):
     template_name = 'announcement_confirm_delete.html'
-    model = Announcement  # Model pro smazání oznámení
-    success_url = reverse_lazy('announcement_list')  # Přesměrování po úspěšném smazání
+    model = Announcement
+    success_url = reverse_lazy('announcement_list')
--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,83 @@
+# from concurrent.futures._base import LOGGER
+#
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.mixins import PermissionRequiredMixin
+# from django.shortcuts import render, redirect, get_object_or_404
+# from django import forms
+# from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
+# from django.urls import reverse_lazy
+#
+# from patients.models import User
+#
+#
+# # Create your views here.
+# # Formulář pro model uživatele
+# class UserForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = '__all__'  # Všechna pole modelu budou zahrnuta ve formuláři
+#         labels = {
+#             'name': 'Jméno',
+#             'surname': 'Příjmení',
+#             'email': 'Email',
+#             'birth_date': 'Datum narození',
+#             'birth_number': 'Rodné číslo',
+#             'insurance': 'Pojišťovna',
+#             'gender': 'Pohlaví',
+#             'role_patient': 'Role uživatele',
+#             'address': 'Adresa',
+#             'contact': 'Kontakt'
+#         }
+#         help_texts = {
+#             'birth_date': 'Zadejte datum ve formátu RRRR-MM-DD.'  # Pomocný text pro pole datum narození
+#         }
+#
+#
+# class UserListView(PermissionRequiredMixin, ListView):
+#     model = User
+#     template_name = 'user_list.html'  # Šablona pro zobrazení seznamu uživatelů
+#     context_object_name = 'patients'
+#     permission_required = 'patients.view_user'  # Požadované oprávnění pro zobrazení seznamu uživatelů
+#
+#     def get_queryset(self):
+#         # Filtrovat uživatele podle role, pokud je specifikována v GET parametru
+#         role = self.request.GET.get('role')
+#         users = User.get_sorted_users()  # Použití statické metody pro lokalizované řazení
+#         if role:
+#             users = filter(lambda user: user.role_patient.lower() == role.lower(), users)
+#         return users
+#
+#
+# class UserDetailView(DetailView):
+#     model = User
+#     template_name = 'user_detail.html'  # Šablona pro zobrazení detailu uživatele
+#     context_object_name = 'user'
+#
+#     def get_object(self):
+#         # Načtení uživatele podle ID
+#         return get_object_or_404(User, pk=self.kwargs['pk'])
+#
+#
+# class UserCreateView(CreateView):
+#     model = User
+#     form_class = UserForm  # Formulář použitý pro vytvoření uživatele
+#     template_name = 'create_user.html'
+#     success_url = reverse_lazy('user_list')  # URL pro přesměrování po úspěšném vytvoření
+#     permission_required = 'patients.add_user'  # Požadované oprávnění pro vytvoření uživatele
+#
+#     def form_invalid(self, form):
+#         LOGGER.warning('User provided invalid data.')  # Zaznamenání varování, pokud je formulář neplatný
+#         return super().form_invalid(form)
+#
+#
+# class UserUpdateView(UpdateView):
+#     template_name = 'create_user.html'
+#     form_class = UserForm  # Formulář použitý pro úpravu uživatele
+#     model = User
+#     success_url = reverse_lazy('user_list')  # URL pro přesměrování po úspěšné úpravě
+#     permission_required = 'patients.change_user'  # Požadované oprávnění pro úpravu uživatele
+
+
 from concurrent.futures._base import LOGGER
 
 from django.contrib.auth.forms import UserCreationForm
@@ -6,11 +86,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django import forms
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
 from django.urls import reverse_lazy
+from django.db.models import Q
 
-from patients.models import User
+from patients.models import User, Role
 
 
-# Create your views here.
 # Formulář pro model uživatele
 class UserForm(forms.ModelForm):
     class Meta:
@@ -40,12 +120,29 @@ class UserListView(PermissionRequiredMixin, ListView):
     permission_required = 'patients.view_user'  # Požadované oprávnění pro zobrazení seznamu uživatelů
 
     def get_queryset(self):
-        # Filtrovat uživatele podle role, pokud je specifikována v GET parametru
+        # Získání parametrů z GET
         role = self.request.GET.get('role')
-        users = User.get_sorted_users()  # Použití statické metody pro lokalizované řazení
-        if role:
-            users = filter(lambda user: user.role_patient.lower() == role.lower(), users)
-        return users
+        search_query = self.request.GET.get('search', '')
+
+        # Filtrovat uživatele podle role
+        if role == 'patient':
+            queryset = User.objects.filter(role_patient=Role.PATIENT)
+        elif role == 'doctor':
+            queryset = User.objects.filter(role_patient=Role.DOCTOR)
+        elif role == 'nurse':
+            queryset = User.objects.filter(role_patient=Role.NURSE)
+        else:
+            # Defaultně zobrazovat všechny uživatele
+            queryset = User.objects.all()
+
+        # Filtrujeme podle vyhledávacího dotazu
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) | Q(surname__icontains=search_query)
+            )
+
+        # Seřadíme výsledky podle příjmení a jména
+        return queryset.order_by('surname', 'name')
 
 
 class UserDetailView(DetailView):
